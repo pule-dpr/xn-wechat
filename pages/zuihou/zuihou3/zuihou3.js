ebc5d4d818e4151c71eea0dc311349e09b92774e
pule <wy_pule@163.com>
2020-11-30 10:49:09 +0800

    first commit

---
+++ pages/zuihou/zuihou3/zuihou3.js
@@ -0,0 +1,77 @@
+// pages/zuihou/zuihou3/zuihou3.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function (event) {
+    console.log("开始分享");
+    if (event.from === "button") {
+      console.log("按钮");
+    } else if (event.from === "menu") {
+      console.log("三个点");
+    }
+
+    return {
+      title: "我是标题",
+      imageUrl: "/assert/p/1.jpg",
+      path: "pages/index/index"
+    }
+  }
+})
